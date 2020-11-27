1df931482dca9dc89e466a2d766348f5b21727c1
Arnold <antaloczi.arnold@gmail.com>
2020-11-27 14:04:50 +0100

    Added eventlistener for buttons

---
+++ app.js
@@ -0,0 +1,106 @@
+"use strict";
+
+const display = document.querySelector(".display");
+const additionBtn = document.querySelector(".addition");
+const subtractionBtn = document.querySelector(".subtraction");
+const multiplicationBtn = document.querySelector(".multiplication");
+const divisionBtn = document.querySelector(".division");
+const oneBtn = document.querySelector(".one");
+const twoBtn = document.querySelector(".two");
+const threeBtn = document.querySelector(".three");
+const fourBtn = document.querySelector(".four");
+const fiveBtn = document.querySelector(".five");
+const sixBtn = document.querySelector(".six");
+const sevenBtn = document.querySelector(".seven");
+const eightBtn = document.querySelector(".eight");
+const nineBtn = document.querySelector(".nine");
+const zeroBtn = document.querySelector(".zero");
+const dotBtn = document.querySelector(".dot");
+const clearBtn = document.querySelector(".clear");
+const equalSignButton = document.querySelector(".equal-sign-button");
+
+const additionAction = () => {
+    
+};
+
+const subtractionAction = () => {
+    
+};
+
+const multiplicationAction = () => {
+    
+};
+
+const divisionAction = () => {
+    
+};
+
+const oneAction = () => {
+    
+};
+
+const twoAction = () => {
+    
+};
+
+const threeAction = () => {
+    
+};
+
+const fourAction = () => {
+    
+};
+
+const fiveAction = () => {
+    
+};
+
+const sixAction = () => {
+    
+};
+
+const sevenAction = () => {
+    
+};
+
+const eightAction = () => {
+    
+};
+
+const nineAction = () => {
+    
+};
+
+const zeroAction = () => {
+    
+};
+
+const dotAction = () => {
+    
+};
+
+const clearAction = () => {
+    
+};
+
+const equalAction = () => {
+    
+};
+
+additionBtn.addEventListener("click", additionAction);
+subtractionBtn.addEventListener("click", subtractionAction);
+multiplicationBtn.addEventListener("click", multiplicationAction);
+divisionBtn.addEventListener("click", divisionAction);
+oneBtn.addEventListener("click", oneAction);
+twoBtn.addEventListener("click", twoAction);
+threeBtn.addEventListener("click", threeAction);
+fourBtn.addEventListener("click", fourAction);
+fiveBtn.addEventListener("click", fiveAction);
+sixBtn.addEventListener("click", sixAction);
+sevenBtn.addEventListener("click", sevenAction);
+eightBtn.addEventListener("click", eightAction);
+nineBtn.addEventListener("click", nineAction);
+zeroBtn.addEventListener("click", zeroAction);
+dotBtn.addEventListener("click", dotAction);
+clearBtn.addEventListener("click", clearAction);
+equalSignButton.addEventListener("click", equalAction);
